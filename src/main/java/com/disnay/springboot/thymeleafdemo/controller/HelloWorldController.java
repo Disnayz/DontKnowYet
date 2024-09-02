d9a82ec1fdc0cf8a079cff563cf5ceb8d4638683
Disnay <adamdemski3@gmail.com>
2024-09-02 23:42:07 +0200

    HelloWorldController

---
+++ src/main/java/com/disnay/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.disnay.springboot.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+    // controller to show a initial HTML form
+    @RequestMapping("/showForm")
+    public String showForm(){
+        return "helloworld-form";
+    }
+    // controller to process form
+}
