231925f72582dd635e100d40a6876cf2486ff118
Disnay <adamdemski3@gmail.com>
2024-11-09 23:43:41 +0100

    Processing form data

--- src/main/java/com/disnay/springboot/thymeleafdemo/controller/StudentController.java
+++ src/main/java/com/disnay/springboot/thymeleafdemo/controller/StudentController.java
@@ -3,16 +3,25 @@ package com.disnay.springboot.thymeleafdemo.controller;
 import com.disnay.springboot.thymeleafdemo.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
-    @Controller
+@Controller
     public class StudentController{
         public String showForm(Model theModel){
             //create a new student Object
             Student theStudent = new Student();
             //add Student Object to the model
             theModel.addAttribute("student",theStudent);
-            //
+
             return "student-form";
+        }
+        @PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") Student theStudent){
+            //log the input data
+            System.out.printf("theStudent " + theStudent.getFirstName() + " " + theStudent.getLastName());
+
+            return "student-confirmation";
         }
     }
 
