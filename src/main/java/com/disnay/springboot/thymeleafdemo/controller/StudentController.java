8e481f64a191c540c9aa1b482c1930f7af5aafe2
Disnay <adamdemski3@gmail.com>
2024-11-10 00:08:56 +0100

    Confirmation

--- src/main/java/com/disnay/springboot/thymeleafdemo/controller/StudentController.java
+++ src/main/java/com/disnay/springboot/thymeleafdemo/controller/StudentController.java
@@ -3,11 +3,13 @@ package com.disnay.springboot.thymeleafdemo.controller;
 import com.disnay.springboot.thymeleafdemo.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
     public class StudentController{
+    @GetMapping("/showStudentForm")
         public String showForm(Model theModel){
             //create a new student Object
             Student theStudent = new Student();
